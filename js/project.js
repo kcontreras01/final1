545878251adfab78cbe95b4ea41cecdf1ff518d4
Kiara Contreras <kcontreras01@gmail.com>
2017-07-19 20:31:27 -0400

    Updating gh-pages from master branch via GitBot

---
+++ js/project.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage();
+});
+
+
+$('#fullpage').fullpage({
+	navigation: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+	menu: '#myMenu',
+	directions: ['up', 'down'],
+	scrollingSpeed: '900',
+	fitToSection: 'true',
+});
+
+window.sr = ScrollReveal();
+sr.reveal('#page1', {duration: 2000});
+
+$(document).ready(function(){
+    jQuery.goup();
+});
