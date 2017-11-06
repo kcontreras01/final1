4897cc5cc13f7b760e32ee1ea3514bbfa27f4693
kcontreras01 <kiara@ool-ad020038.dyn.optonline.net>
2017-11-06 14:24:49 -0500

    css started

--- js/project.js
+++ js/project.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+
+	var anchors = [],
+    tooltips = [];
+$('.section').each(function() {
+    // retrieve the attribute
+    var navigation = $(this).data('navigation');
+    if (!navigation)
+        return;
+
+    // the attribute data-navigation is built like : anchor|tooltip
+    var parts = navigation.split('|');
+
+    anchors.push(parts[0]);
+    tooltips.push(parts[1]);
+});
+
 	$('#fullpage').fullpage();
 });
 
@@ -14,6 +30,8 @@ $('#fullpage').fullpage({
 window.sr = ScrollReveal();
 sr.reveal('#page1', {duration: 2000});
 
-$(document).ready(function(){
-    jQuery.goup();
-});
+
+
+// $(document).ready(function(){
+//     jQuery.goup();
+// });
